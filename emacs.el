fd889c85ad0013707cdd9fa6c60bcc926f3b857a
Fred Brunel <fbrunel@gmail.com>
2008-12-24 03:52:43 +0100

    Cleaned up the aging configuration file. Inspired by the Emacs Starter Kit[1] by Phil Hagelberg.
    [1] http://github.com/technomancy/emacs-starter-kit/

--- emacs.el
+++ emacs.el
@@ -1,24 +1,101 @@
 ;;;; .emacs
 
+;;; Paths
+
+(setq *search-dir* (expand-file-name "~/Library/Emacs/vendor"))
+(setq *dotfiles-dir* (expand-file-name "~/.emacs.d"))
+(add-to-list 'load-path *search-dir*)
+
+;;; Default librairies
+
+(require 'cl)
+(require 'saveplace)
+(require 'ffap)
+(require 'uniquify)
+(require 'ansi-color)
+(require 'recentf)
+
+;;; Modes
+
+(require 'magit)
+
+;; Java
+
+;(require 'cedet)
+;(setq semanticdb-default-save-directory (concat *dotfiles-dir* "semantic/"))
+;(semantic-load-enable-code-helpers)
+;(require 'jde)
+;(add-hook 'jde-mode-hook 'turn-on-auto-fill)
+
+;; Lisp
+;; (require 'slime)
+;; (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
+;; (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
+;; (setq lisp-indent-function 'common-lisp-indent-function)
+
+;; Autoloads
+
+(autoload 'ruby-mode "ruby-mode")
+(autoload 'css-mode "css-mode")
+
+;; Associate modes with file extensions
+
+(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . diff-mode))
+(add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
+;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\(on\\)?$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
+
+;;; Keybindings
+
+(global-set-key [dead-circumflex] 'compose-circumflex-map)
+(global-set-key [home] 'beginning-of-line)
+(global-set-key [end] 'end-of-line)
+(global-set-key [C-kp-home] 'beginning-of-buffer)
+(global-set-key [C-kp-end] 'end-of-buffer)
+(global-set-key [C-return] 'kill-this-buffer)
+(global-set-key [C-tab] 'other-window)
+(global-set-key [C-backspace] 'comment-region)
+(global-set-key [f5] 'jde-ant-build)
+(global-set-key [f7] 'previous-error)
+(global-set-key [f8] 'next-error)
+(global-set-key [f10] 'jde-javadoc-checkdoc-at-line)
+(global-set-key [f11] 'grep-find)
+(global-set-key [C-f12] 'cvs-update)
+(global-set-key [f12] 'add-change-log-entry)
+(global-set-key [C-kp-add] 'enlarge-window)
+(global-set-key [C-kp-subtract] 'shrink-window)
+(global-set-key [kp-delete] 'delete-char)
+
+(define-key global-map [(delete)] "\C-d")
+(define-key global-map "\M-g" 'goto-line)
+
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-x m") 'eshell)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;;; Customization
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(add-log-mailing-address nil)
- '(browse-url-browser-function (quote browse-url-galeon))
- '(browse-url-galeon-new-window-is-tab t)
- '(browse-url-new-window-flag t)
- '(canlock-password "7cd01b64aef323dbbda6f2378bc925862bf5946a")
+ '(auto-compression-mode t)
+ '(blink-cursor-mode nil)
  '(column-number-mode t)
  '(compilation-window-height 10)
  '(current-language-environment "Latin-1")
  '(cursor-in-non-selected-windows nil)
  '(default-frame-alist (quote ((foreground-color . "cornsilk") (background-color . "#21515c") (cursor-color . "white") (vertical-scroll-bars . right) (tool-bar-lines . 0) (menu-bar-lines . 1))))
  '(delete-selection-mode t nil (delsel))
- '(diary-file "~/.emacs.d/diary")
  '(dired-listing-switches "-l")
- '(erc-nick "Bloody")
+ '(erc-nick "fredb")
  '(erc-server "ircnet.grolier.net")
  '(global-font-lock-mode t nil (font-lock))
  '(gnus-group-mode-hook (quote (gnus-topic-mode)))
@@ -33,6 +110,7 @@
  '(icomplete-mode t)
  '(ido-everywhere t)
  '(ido-mode (quote both) nil (ido))
+ '(inhibit-startup-screen t)
  '(ispell-program-name "aspell" t)
  '(jde-ant-enable-find t)
  '(jde-ant-read-target t)
@@ -44,11 +122,13 @@
  '(mail-source-directory "~/.emacs.d/Mail/")
  '(mail-user-agent (quote message-user-agent))
  '(read-mail-command (quote gnus))
+ '(recentf-mode t)
  '(scroll-bar-mode (quote right))
  '(show-paren-mode t)
  '(slime-conservative-indentation nil)
  '(slime-startup-animation nil)
  '(tool-bar-mode nil nil (tool-bar))
+ '(transient-mark-mode t)
  '(truncate-lines t)
  '(user-full-name "Frederic Brunel")
  '(user-mail-address "fbrunel@gmail.com"))
@@ -58,7 +138,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(default ((t (:stipple nil :background "#21515c" :foreground "cornsilk" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :family "andale mono"))))
- '(cursor ((t (:background "white"))) t)
+ '(cursor ((t (:background "white"))))
  '(font-lock-builtin-face ((t (:foreground "OliveDrab1"))))
  '(font-lock-comment-face ((t (:foreground "RosyBrown1" :background "#005f68"))))
  '(font-lock-constant-face ((t (:foreground "Violet"))))
@@ -92,82 +172,5 @@
  '(show-paren-match ((((class color)) (:background "#005fbb"))))
  '(slime-repl-inputed-output-face ((((class color) (background dark)) (:foreground "darkgray"))))
  '(speedbar-highlight-face ((((class color) (background dark)) (:background "sea green")))))
-
-;;; Load paths
-(require 'cl)
-
-(dolist (file (directory-files (expand-file-name "~/Library/Emacs/") t "[a-z]"))
-  (pushnew file load-path))
-(pushnew "/usr/local/bin" exec-path)
-
-;;; Modes
-
-(require 'psvn)
-
-;; CEDET
-;(require 'cedet)
-;(setq semanticdb-default-save-directory "/Users/fbrunel/.emacs.d/semantic/")
-;(semantic-load-enable-code-helpers)
-
-;; Rails
-
-(autoload 'ruby-mode "ruby-mode" "Ruby editing mode." t)
-(setq auto-mode-alist  (cons '("\\.rb$" . ruby-mode) auto-mode-alist))
-(setq auto-mode-alist  (cons '("\\.rhtml$" . html-mode) auto-mode-alist))
-(modify-coding-system-alist 'file "\\.rb$" 'utf-8)
-(modify-coding-system-alist 'file "\\.rhtml$" 'utf-8)
-
-;(require 'rails)
-
-;; JDE
-;(require 'jde)
-;(add-hook 'jde-mode-hook 'turn-on-auto-fill)
-
-;; ibuffer
-(require 'ibuffer)
-(global-set-key [(control x) (control b)] 'ibuffer)
-
-;; Bookmark
-;; (require 'bm)
-;; (global-set-key [C-f2] 'bm-toggle-bookmark)
-;; (global-set-key [f2] 'bm-goto-bookmark)
-;; (global-set-key [S-f2] 'bm-goto-bookmark-previous)
-
-;; Lisp
-;; (require 'slime)
-;; (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
-;; (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
-;; (setq lisp-indent-function 'common-lisp-indent-function)
-
-;; Text mode
-;(add-hook 'text-mode-hook 'turn-on-auto-fill)
-
-;; CSS mode
-(autoload 'css-mode "css-mode")
-(setq auto-mode-alist (cons '("\\.css\\'" . css-mode) auto-mode-alist))
-(setq cssm-indent-function #'cssm-c-style-indenter)
-
-;;; Globals keybindings
-(global-set-key [dead-circumflex] 'compose-circumflex-map)
-(global-set-key [home] 'beginning-of-line)
-(global-set-key [end] 'end-of-line)
-(global-set-key [C-kp-home] 'beginning-of-buffer)
-(global-set-key [C-kp-end] 'end-of-buffer)
-(global-set-key [C-return] 'kill-this-buffer)
-(global-set-key [C-tab] 'other-window)
-(global-set-key [C-backspace] 'comment-region)
-(global-set-key [f5] 'jde-ant-build)
-(global-set-key [f7] 'previous-error)
-(global-set-key [f8] 'next-error)
-(global-set-key [f10] 'jde-javadoc-checkdoc-at-line)
-(global-set-key [f11] 'grep-find)
-(global-set-key [C-f12] 'cvs-update)
-(global-set-key [f12] 'add-change-log-entry)
-(global-set-key [C-kp-add] 'enlarge-window)
-(global-set-key [C-kp-subtract] 'shrink-window)
-(global-set-key [kp-delete] 'delete-char)
-
-(define-key global-map [(delete)] "\C-d")
-(define-key global-map "\M-g" 'goto-line)
 
 ;;;; .emacs ends here
